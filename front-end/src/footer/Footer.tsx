141635a48aab97c6a9fce16ff41112463c1fe7be
ricardobar96 <ricardobar96@gmail.com>
2023-09-16 00:27:00 +0100

    Implemented footer

---
+++ front-end/src/footer/Footer.tsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+
+    return (
+        <footer className="fixed-bottom bg-white text-center text-lg-start">
+        <div className="text-white text-center p-3 bg-secondary">
+          © 2022 Copyright:
+          <a className="text-white text-decoration-none" href="https://es.linkedin.com/in/ricardo-baloira-armas-11188b100">
+              &nbsp;&nbsp;Ricardo Baloira Armas
+          </a>
+        </div>
+      </footer>
+    )
+}

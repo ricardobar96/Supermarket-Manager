625c2e4b67acc1bd98713f166125bf8c6f5c7f1f
ricardobar96 <ricardobar96@gmail.com>
2023-09-17 18:16:37 +0100

    Changes style footer

--- front-end/src/footer/Footer.tsx
+++ front-end/src/footer/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
         <footer className="fixed-bottom bg-white text-center text-lg-start">
         <div className="text-white text-center p-3 bg-secondary">
           © 2022 Copyright:
-          <a className="text-white text-decoration-none" href="https://es.linkedin.com/in/ricardo-baloira-armas-11188b100">
+          <a className="text-white text-decoration-none fw-bold" href="https://es.linkedin.com/in/ricardo-baloira-armas-11188b100">
               &nbsp;&nbsp;Ricardo Baloira Armas
           </a>
         </div>

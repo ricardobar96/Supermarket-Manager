88e77cdd6b76540f9775c84ef067a9159a5e33ce
ricardobar96 <ricardobar96@gmail.com>
2023-09-17 19:20:11 +0100

    Changed card product layout, buttons redirecting on cards

--- front-end/src/v1/ManageProducto_v1.tsx
+++ front-end/src/v1/ManageProducto_v1.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import { Link, Route, BrowserRouter, Routes, useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
 import Productos_v1 from './Producto_v1';
+import { Button } from 'react-bootstrap';
 
 interface IState { producto?: Supermercado.Producto }
 
@@ -17,12 +18,19 @@ declare module Supermercado {
 }
 
 export default function ManageProducto_v1() {
+
     let idP: React.RefObject<HTMLInputElement> = useRef<HTMLInputElement>(null);
     let nombreP: React.RefObject<HTMLInputElement> = useRef<HTMLInputElement>(null);
     let precioP: React.RefObject<HTMLInputElement> = useRef<HTMLInputElement>(null);
     let stockP: React.RefObject<HTMLInputElement> = useRef<HTMLInputElement>(null);
 
     let navigate = useNavigate();
+
+    const routeOrder = () =>{ 
+        let path = `/api/v1/productos`; 
+        navigate(path);
+    }
+
     const [stProducto, setStProducto] = useState<IState>({});
     const { id } = useParams();
 
@@ -45,19 +53,13 @@ export default function ManageProducto_v1() {
                     <div className="col-md-12 col-lg-4 mb-4 mb-lg-0">
                         <div className="card " style={{background: "#d6eaf8"}}>
                             <div className="d-flex justify-content-between p-3">
-                                <p className="lead mb-0">Product details</p>
-                                    <div className="bg-info rounded-circle d-flex align-items-center justify-content-center shadow-1-strong"
-                                    style={{width: "35px", height: "35px"}}>
-                                        <p className="text-white mb-0 small">x{stProducto.producto?.stock}</p>
-                                    </div>
+                            <Button className="btn btn-danger btn-md" onClick={routeOrder}>Return</Button>
+                            <button className="btn btn-success btn-md" onClick={routeOrder}>Order!</button>
+                                    
                                 </div>
                             <img src="https://www.kesargrocery.com/images/P/Wonderful%20Pistachios%2C%20No%20Shell%20Nuts%2C%20Variety%20Pack%20%28Pack%20of%209%29.webp"
-                            className="card-img-top" style={{height: "300px"}} alt="Laptop" />
+                            className="card-img-top" style={{height: "300px"}} alt="Product img" />
                             <div className="card-body">
-                                <div className="d-flex justify-content-between">
-                                    <p className="small"><a href="/api/v1/productos" className="text-danger text-decoration-none">Return to catalog</a></p>
-                                    <p className="small text-danger"><s>{stProducto.producto?.preciounidad} €</s></p>
-                                </div>
 
                             <div className="d-flex justify-content-between mb-3">
                                 <h5 className="mb-0">{stProducto.producto?.nombre}</h5>
